564b66f324e2192bcf249091640b12f46dc725b4
natan <natan.muller@auxiliadorapredial.com.br>
2026-07-03 12:28:48 -0300

    chore: remover bibliotecas desnecessarias

--- src/Repositories/CondominioRepository.cs
+++ src/Repositories/CondominioRepository.cs
@@ -2,8 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using System.IO; 
-using System.Text.Json; 
 using AuxiAPI.src.Entities;
 using AuxiAPI.src.Contexts;
 using Microsoft.EntityFrameworkCore;
